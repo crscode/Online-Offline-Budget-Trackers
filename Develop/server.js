d7d079457c25a55b0bcb890b35ce8099f57e4d19
Christoper Smith <Christophersmithcode@gmail.com>
2020-07-30 13:18:24 -0400

    changed port

--- Develop/server.js
+++ Develop/server.js
@@ -2,25 +2,21 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
-const PORT = 3000;
-
+const PORT = process.env.PORT || 8000;
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes
+
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
