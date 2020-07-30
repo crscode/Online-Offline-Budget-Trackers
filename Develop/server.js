b7bc38c07befd18525b1502ea5c3586bae9a5317
Christoper Smith <Christophersmithcode@gmail.com>
2020-07-30 13:42:29 -0400

    manifest and webmanifast

--- Develop/server.js
+++ Develop/server.js
@@ -2,21 +2,25 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = process.env.PORT || 8000;
+
+const PORT = 8000;
+
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds231725.mlab.com:31725/heroku_sqc6mlww", {
+mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-
+// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
